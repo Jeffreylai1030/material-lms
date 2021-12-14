c59bc818e750424c1f65a45f422d30c485779364
Jeffrey Lai <52941729+Jeffreylai1030@users.noreply.github.com>
2021-12-14 21:49:11 +0800

    Add date range on dashboard

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MaterialLms
+# Library Management System
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.10.
 
@@ -8,7 +8,7 @@ Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app w
 
 ## Code scaffolding
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+Run `ng generate component views/{component-name}` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
 ## Build
 
@@ -22,6 +22,5 @@ Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.
 
 Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
 
-## Further help
+## Deploy to Firestore
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -3,10 +3,11 @@ import { BorrowService } from 'src/app/services/borrow.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { EChartsOption } from 'echarts';
 import { BookService } from 'src/app/services/book.service';
-import { DatePipe } from '@angular/common';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { FormControl, FormGroup } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
@@ -21,21 +22,24 @@ export class DashboardComponent implements OnInit {
     private datepipe: DatePipe,
   ) {}
 
-  @ViewChild(MatPaginator, { static: true })
-  paginator!: MatPaginator;
-  @ViewChild(MatSort, { static: true })
-  sort!: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
   dataSource!: MatTableDataSource<any>;
 
+  today = new Date();
+  chartTitle = 'New Borrowed Last 7 Days';
   totalBooksNumber = 0;
   totalBorrowsNumber = 0;
   totalTodayBorrowsNumber = 0;
   totalBorrowsExpiredNumber = 0;
   borrows: BorrowDto[] = [];
-  xAxis: string[] = [];
-  yAxis: number[] = [];
   chartOption: EChartsOption = {};
-  displayedColumns = ['date', 'number']
+  displayedColumns = ['date', 'number'];
+  selectedTab = 'lastWeek';
+  dateRange = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl()
+  });
 
   ngOnInit() {
     this.bookService.get().subscribe(item => {
@@ -43,69 +47,102 @@ export class DashboardComponent implements OnInit {
     })
 
     this.borrowService.getByStatus('0').subscribe(item => {
-      let data: any = [];
+
       this.borrows = item;
       this.totalBorrowsNumber = item.length;
       this.totalTodayBorrowsNumber = item.filter(x => x.addDate.toDate().toISOString().substring(0, 10) === new Date().toISOString().substring(0, 10)).length;
       this.totalBorrowsExpiredNumber = item.filter(x => x.dueDate.toDate().toISOString().substring(0, 10) <= new Date().toISOString().substring(0, 10)).length;
 
-      let count = 6;
-      while (count >= 0) {
-        const date = new Date();
-        const pastDate = new Date(date.setDate(date.getDate() - count)).toISOString().substring(0, 10);
-        const pastData = item.filter(x => x.addDate.toDate().toISOString().substring(0, 10) === pastDate).length;
-        this.xAxis.push(pastDate);
-        this.yAxis.push(pastData);
-        data.push({ date: pastDate, number: pastData });
-        count--;
-      }
-
-
-
-      this.dataSource = new MatTableDataSource(data);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-
-      this.chartOption = {
-        legend: {
-          data: ['Books']
-        },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'line',
-            label: {
-              backgroundColor: '#6a7985'
-            }
-          }
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: {
-          type: 'category',
-          data: this.xAxis, // Past 7 days Date
-          axisTick: {
-            alignWithLabel: true
-          }
-        },
-        yAxis: {
-          type: 'value',
-        },
-        series: [
-          {
-            name: 'Books',
-            data: this.yAxis, // Number of books
-            type: 'line',
-          },
-        ],
-      };
-
+      this.onTabChange('lastWeek');
     })
+
   }
 
+  onTabChange(value: string) {
+    const endDate = new Date(new Date().setDate(new Date().getDate() + 1));
+
+    if (value === 'lastWeek') {
+      this.chartTitle = 'New Borrowed Last 7 Days';
+      const startDate = new Date(new Date().setDate(new Date().getDate() - 6));
+      this.generateLineChart(startDate, endDate);
+    } else if (value === 'lastMonth') {
+      this.chartTitle = 'New Borrowed Last 30 Days';
+      const startDate = new Date(new Date().setDate(new Date().getDate() - 29));
+      this.generateLineChart(startDate, endDate);
+    }
+  }
+
+  onChartChange() {
+    const start: Date = this.dateRange.value.start;
+    const end: Date = this.dateRange.value.end;
+    const startDate: Date = new Date(start.setDate(start.getDate() + 1));
+    const endDate: Date = new Date(end.setDate(end.getDate() + 2));
+    this.selectedTab = '';
+    this.chartTitle = `New Borrowed ${this.datepipe.transform(startDate, 'mediumDate')} ~ ${this.datepipe.transform(endDate, 'mediumDate')}`;
+    this.generateLineChart(startDate, endDate);
+  }
+
+  generateLineChart(startDate: Date, endDate: Date) {
+    let data: any = [];
+    let xAxis: string[] = [];
+    let yAxis: number[] = [];
+
+    let startDateStr = startDate.toISOString().substring(0, 10);
+    let endDateStr = endDate.toISOString().substring(0, 10);
+
+    console.log('start: ' + startDateStr, 'end: ' + endDateStr);
+
+    while (startDateStr !== endDateStr) {
+      const date = new Date(startDateStr);
+      const pastDate = date.toISOString().substring(0, 10);
+      const pastData = this.borrows.filter(x => x.addDate.toDate().toISOString().substring(0, 10) === pastDate).length;
+      xAxis.push(pastDate);
+      yAxis.push(pastData);
+      data.push({ date: pastDate, number: pastData });
+      startDateStr = new Date(date.setDate(date.getDate() + 1)).toISOString().substring(0, 10);
+    }
+
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+    this.chartOption = {
+      legend: {
+        data: ['Books']
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'line',
+          label: {
+            backgroundColor: '#6a7985'
+          }
+        }
+      },
+      // grid: {
+      //   left: '3%',
+      //   right: '4%',
+      //   bottom: '3%',
+      //   containLabel: true
+      // },
+      xAxis: {
+        type: 'category',
+        data: xAxis, // Past 7 days Date
+        axisTick: {
+          alignWithLabel: true
+        }
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name: 'Books',
+          data: yAxis, // Number of books
+          type: 'line',
+        },
+      ],
+    };
+  }
 
 }
